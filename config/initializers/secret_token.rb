c95bb1bc2eb07edd81a752607b79bc39ba869408
Thiago Verney <thiago.verney@gmail.com>
2013-03-03 22:04:36 -0300

    Created a rails application and statuses updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feirafacil::Application.config.secret_token = 'c9904921a10450686d5c7af19d0fc44ef345a976ee4aa7bad001db9718fdf05b14692999261d43490c7212f521411d0d8975cccc146523a99fc5ae161c786255'
